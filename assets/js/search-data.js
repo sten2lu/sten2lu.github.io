6f47e6e304fa9f5462df6de54115004b4999c93a
sten2lu <sten2lu@users.noreply.github.com>
2026-07-07 10:10:15 +0000

    Deploying to gh-pages from @ sten2lu/sten2lu.github.io@e56791c2552ffc92f874b9736c7e76913b04035f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,7 +121,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/awesome_cv_academic.pdf", "_blank");
         },
       },{
         id: 'social-email',
